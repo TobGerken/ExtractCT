47a4d6b07e593acbea37fd4de147910d8f64f057
Tobias Gerken <tobias.gerken@live.de>
2019-10-14 12:33:25 -0400

    add runscript

---
+++ Matlab/ExtractCT/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#PBS -l nodes=1:ppn=1
+
+#PBS -l walltime=24:00:00
+
+#PBS -A open
+
+# Get started
+
+echo "Job started on ‘hostname‘ at ‘date‘"
+
+# Load in matlab
+
+module purge
+
+module load matlab/R2016a
+
+# Go to the correct place
+
+cd $PBS_O_WORKDIR
+
+# Run the job itself - a matlab script called runThis.m
+
+matlab-bin -nodisplay -nosplash < ExtractCT.m > log.matlabRun
+
+# Finish up
+
+echo "Job Ended at ‘date‘"
